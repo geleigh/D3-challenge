4ed150874bad91806fbff1ba8227907cc412a517
geleigh <57293957+geleigh@users.noreply.github.com>
2020-04-19 18:45:04 -0400

    push changes
    push changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+// D3 Homework - Data-Journalism
+
+// Setup Chart Parameters/Dimensions
+var svgWidth = 1920;
+var svgHeight = 1000;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 90,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
